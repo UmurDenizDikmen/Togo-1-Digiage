a077a4acf3a4005c0511c7b715a9a03c9fa9dac4
UmurDenizDikmen <umurdenizdikmen@gmail.com>
2023-04-26 23:37:31 +0300

    systemevolved

--- Assets/Scripts/BoxController.cs
+++ Assets/Scripts/BoxController.cs
@@ -7,6 +7,7 @@ public class BoxController : MonoBehaviour
 {
     GameManager _gameManager;
     private int maxCapacity = 3;
+    private GameObject objects;
     private void Start()
     {
         _gameManager = GameManager.instance;
@@ -19,8 +20,21 @@ public class BoxController : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit))
         {
-            var objects =   Instantiate(_gameManager.carringObjects[randomObjects],hit.point,Quaternion.identity);
-            objects.transform.SetParent(transform);
+           var type = _gameManager.carringObjects[randomObjects].GetComponent<DraggableItem>().itemType;
+           switch(type)
+           {
+               case DraggableItem.typeOfItem.waterBottle :
+                 objects =   Instantiate(_gameManager.carriableObjects[0],hit.point,Quaternion.identity);
+               break;
+                 case DraggableItem.typeOfItem.cips :
+                 objects =   Instantiate(_gameManager.carriableObjects[1],hit.point,Quaternion.identity);
+               break;
+                 case DraggableItem.typeOfItem.cola :
+                 objects =   Instantiate(_gameManager.carriableObjects[2],hit.point,Quaternion.identity);
+               break;
+           }
+
+             objects.transform.SetParent(transform);
             objects.transform.DOScale(new Vector3(0.2f,0.2f,0.2f),0.01f);
             _gameManager.carringObjects.Remove(objects);
 
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,7 @@ public class GameManager : MonoBehaviour
     public List <int> otomatAvaliblePos = new List<int>{1,2,3,4,5,6,7,8,9};
     public List <int> removeNumbers = new List<int>(); //bunu yapay zaeka oldugu zaman otomat lsitesine tekrar eklicez.
     public List <GameObject> carringObjects = new List<GameObject>();
+    public List <GameObject> carriableObjects = new List<GameObject>();
     public int currentOtomatPos;
     public TextMeshProUGUI currentPosNumberText;
     public static GameManager instance;
